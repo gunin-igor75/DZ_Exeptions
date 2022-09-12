186f38ffa201dd5f043e2d73d18243936b08ff89
Igor Gunin <gunin-igor75@yandex.ru>
2022-09-12 19:06:40 +0600

    add files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static boolean check(String login, String password, String confirmPassword) {
+        String sample = "\\w+"; // латинские буквы, цифры и знак подчеркивания
+        boolean resultLogin = login.matches(sample) && login.length() <= 20;
+        boolean resultPassword = login.matches(sample) && login.length() < 20 && password.equals(confirmPassword);
+        if (!resultLogin) {
+            try {
+                new WrongLoginException();
+            } catch (WrongLoginException e) {
+                return false;
+            }
+        }
+        if (!resultPassword) {
+            try {
+                throw new WrongPasswordException();
+            } catch (WrongPasswordException e) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(check("ava_skyprogo", "D_1hWiKjjP_9", "D_1hWiKjjP_9"));
+        System.out.println(check("ava_skyp,rogo", "D_1hWiKjjP_9", "D_1hWiKjjP_9"));
+        System.out.println(check("ava_skypzazazazazzazazarogo", "D_1hWiKjjP_9", "D_1hWiKjjP_9"));
     }
 }
